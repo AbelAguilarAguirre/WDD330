b7b220786676e627aab494a90d790875143d9eec
AbelAguilarAguirre <ulysses.aguilar.aguirre@gmail.com>
2023-03-04 01:04:46 -1000

    header and footer wip

---
+++ scripts/main.js
@@ -0,0 +1,21 @@
+import {
+    loadTemplate,
+    renderWithTemplate
+} from './utils.mjs'
+
+displayHeaderFooter();
+
+async function displayHeaderFooter() {
+    const header = await loadTemplate('../templates/header.html');
+    const footer = await loadTemplate('../templates/footer.html');
+    renderWithTemplate(header, document.getElementById('header'), 'beforebegin', true);
+    renderWithTemplate(footer, document.getElementById('footer'), 'beforebegin', true);
+    let hambutton = document.querySelector('.ham');
+    let mainnav = document.querySelector('nav');
+    hambutton.addEventListener('click', () => { mainnav.classList.toggle('responsive') }, false);
+}
+
+//Hamburger button
+
+
+window.onresize = () => { if (window.innerWidth > 760) mainnav.classList.remove('responsive') };
