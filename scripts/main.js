00b131b99c3325a21ff2fe88aad7297c5faf75db
AbelAguilarAguirre <ulysses.aguilar.aguirre@gmail.com>
2023-03-04 01:34:38 -1000

    before end

--- scripts/main.js
+++ scripts/main.js
@@ -9,7 +9,7 @@ async function displayHeaderFooter() {
     const header = await loadTemplate('templates/header.html');
     const footer = await loadTemplate('templates/footer.html');
     renderWithTemplate(header, document.getElementById('header'), 'beforebegin', true);
-    renderWithTemplate(footer, document.getElementById('footer'), 'beforebegin', true);
+    renderWithTemplate(footer, document.getElementById('footer'), 'beforeend', true);
     let hambutton = document.querySelector('.ham');
     let mainnav = document.querySelector('nav');
     hambutton.addEventListener('click', () => { mainnav.classList.toggle('responsive') }, false);
