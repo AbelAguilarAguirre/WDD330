d1d909098323fff3b7d05edf8e16e1a7485e832c
AbelAguilarAguirre <ulysses.aguilar.aguirre@gmail.com>
2023-03-17 01:52:19 -1000

    picture fade home banner img

--- scripts/main.js
+++ scripts/main.js
@@ -13,12 +13,55 @@ async function displayHeaderFooter() {
     let dropdown = document.querySelector('.dropbtn');
     let dropdownContent = document.querySelector('.dropdown-content');
     dropdown.addEventListener('click', () => { dropdownContent.classList.toggle('responsive') }, false);
-    var wrapper = document.querySelector('nav');
+    let wrapper = document.querySelector('nav');
     document.addEventListener('click', function (e) {
         if (!wrapper.contains(e.target)) {
             dropdownContent.classList.remove('responsive');
             dropdown.classList.remove('change');
-        }
-    })
-}
+        };
+    });
+};
+buildImageTransition();
+function buildImageTransition() {
+    let imgTemplate = '';
+    let imgContainer = document.getElementById('hero-banner');
+    for (let i = 1; i < 43; i++) {
+        let imageSrc = `../images/full tables/${i}.webp`;
+        imgTemplate += `<img src='${imageSrc}' alt='Full Candy Table' class='fadeImg'>`;
+    };
+    imgTemplate += `<a href="custom-tables.html">
+    <h2>Book With Us Today</h2>
+</a>`
+    imgContainer.innerHTML = imgTemplate;
+    // let images = document.getElementsByClassName('fadeImg');
+    // for (let i = 0; i < images.length; i++) {
+    //     images[i].style.visibility = 'visible';
+    //     images[i].style.opacity = 1;
+    //     images[i].style.transition = 'all .5s'
+    // }
+    // let top = 0;
+    // let a = document.querySelector("#hero-banner > a");
+    // let cur = images.length - 1;
+    // a.style.zIndex = top + 2;
+    // setInterval(changeImage, 5000);
+    // setInterval(changeVisiblility, 5100);
+    // function changeImage() {
 
+    //     let nextImage = (1 + cur) % images.length;
+
+    //     images[cur].style.zIndex = top + 1;
+    //     images[nextImage].style.zIndex = top;
+    //     images[cur].style.zIndex = top;
+
+    //     top = top + 1;
+
+    //     images[nextImage].style.visibility = 'hidden';
+
+    //     cur = nextImage;
+
+    // }
+    // function changeVisiblility() {
+        
+    //     images[cur].style.visibility= 'visible';
+    // }
+}
