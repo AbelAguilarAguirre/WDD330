39546569f76b7bfd4b04d68930f0e30c85176c30
AbelAguilarAguirre <ulysses.aguilar.aguirre@gmail.com>
2023-03-22 23:11:22 -1000

    orders

--- scripts/packages.js
+++
@@ -1,8 +0,0 @@
-let radio = document.querySelector(".hidden");
-let packageOption = document.querySelector(".packageOption");
-function highlight(e) {
-    if(e)
-       {e.classList.add("active")}
-    else
-       return
- }
