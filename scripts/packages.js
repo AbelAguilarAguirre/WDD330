9bd5539d2d737b33d4493142ecc6244fcc099f45
AbelAguilarAguirre <ulysses.aguilar.aguirre@gmail.com>
2023-03-22 17:12:49 -1000

    WIP Package Page

---
+++ scripts/packages.js
@@ -0,0 +1,8 @@
+let radio = document.querySelector(".hidden");
+let packageOption = document.querySelector(".packageOption");
+function highlight(e) {
+    if(e)
+       {e.classList.add("active")}
+    else
+       return
+ }
