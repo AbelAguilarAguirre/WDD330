ca9afb66652e30a1b54873a3dcba78b6b4263a05
AbelAguilarAguirre <ulysses.aguilar.aguirre@gmail.com>
2023-03-29 23:03:08 -1000

    Gallery WIP

--- scripts/gallery.js
+++ scripts/gallery.js
@@ -7,27 +7,25 @@ fetch(requestURL)
   })
   .then(function (jsonObject) {
     const images = jsonObject['Images'];
-    console.log(images);
     images.forEach(displayImages);
   });
 function displayImages(image) {
   let imgTemplate = '';
-  console.log(image),
-    image.FullTable.forEach(image => {
+  image.FullTable.forEach(image => {
 
-      imgTemplate +=
-        `<div class="imageSets" >
+    imgTemplate +=
+      `<div class="imageSets" >
        <img class="mainimg" src="${image.TableImg}" alt="" loading="lazy">
        `;
-      if ( image.Set != undefined) {
-        for (let i = 0; i < image.Set.length; i++) {
-          imgTemplate += `<img class="childimg" src="${image.Set[i].Src}" alt="${image.Set[i].Title}">`
-        }
+    if (image.Set != undefined) {
+      for (let i = 0; i < image.Set.length; i++) {
+        imgTemplate += `<img class="childimg" src="${image.Set[i].Src}" alt="${image.Set[i].Title}">`
       }
+    }
 
 
-      imgTemplate += `</div>`;
-    })
+    imgTemplate += `</div>`;
+  })
   imgTemplate +=
     `<hr></div>`
   imageCards.innerHTML += imgTemplate;
